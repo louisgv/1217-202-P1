9f6bf730aa4761eb153e58d45a0e0a916c8ff0cf
Lab Nguyen (RIT Student) <hhn2884@ad.rit.edu>
2017-09-14 19:14:58 -0400

    Initialize the project

---
+++ Nguyen_Random/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+    public GameObject[] cameras;
+
+    private int currentCameraIndex;
+
+    /// <summary>
+    /// Initialize current index, disable all camera except the first
+    /// </summary>
+    private void Start()
+    {
+        currentCameraIndex = 0;
+
+        foreach (var camera in cameras)
+        {
+            camera.SetActive(false);
+        }
+
+        if (cameras.Length > 0)
+        {
+            cameras[0].SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// Cycle through the available camera in the camera array
+    /// </summary>
+    /// <returns>The next camera index</returns>
+    private int GetNextCameraIndexCyclic()
+    {
+        // Cycle index using mod op
+        return (currentCameraIndex + 1) % cameras.Length;
+    }
+
+    /// <summary>
+    /// Disable camera at specified index
+    /// </summary>
+    /// <param name="index">camera index</param>
+    private void DisableCamera(int index)
+    {
+        cameras[index].SetActive(false);
+    }
+
+    /// <summary>
+    /// Find the next camera and enable it
+    /// </summary>
+    /// <returns>The index of the camera enabled</returns>
+    private int EnableNextCamera()
+    {
+        int nextCameraIndex = GetNextCameraIndexCyclic();
+
+        cameras[nextCameraIndex].SetActive(true);
+
+        return nextCameraIndex;
+    }
+
+    /// <summary>
+    /// Check for C key and cycle through the list of available camera
+    /// </summary>
+    private void Update()
+    {
+        bool cKeyDown = Input.GetKeyDown(KeyCode.C);
+        if (cKeyDown)
+        {
+            DisableCamera(currentCameraIndex);
+            currentCameraIndex = EnableNextCamera(); // Mutate currentCameraIndex
+        }
+    }
+
+    /// <summary>
+    /// Showing camera cycling instruction and the current camera name
+    /// </summary>
+    private void OnGUI()
+    {
+        GUI.skin.box.wordWrap = true;
+
+        GUI.Box(new Rect(0, 10, 180, 54), "Current Camera:\n" + cameras[currentCameraIndex].name + ".");
+
+        GUI.Box(new Rect(Screen.width - 190, 10, 180, 36), "Press the 'c' key to switch camera.");
+    }
+
+}
---
+++ Nguyen_Random/Assets/Scripts/PerlinTerrain.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Author: LAB
+/// Description: Generate perlin terrain and assign it to the terrain height map
+/// Restriction: Requires the GameObject to have TerrainCollider
+/// </summary>
+
+[RequireComponent(typeof(TerrainCollider))]
+public class PerlinTerrain : MonoBehaviour
+{
+    public int resolution = 129;
+
+    public float scale = 4.5f;
+
+    public Vector3 worldSize = new Vector3(200, 50, 200);
+
+    private TerrainData terrainData;
+
+    private float xOrigin;
+
+    private float yOrigin;
+
+    /// <summary>
+    /// Assign a random perlin origin and get terrainData
+    /// </summary>
+    private void Awake()
+    {
+        xOrigin = Random.value * resolution;
+
+        yOrigin = Random.value * resolution;
+
+        terrainData = GetComponent<TerrainCollider>().terrainData;
+    }
+
+    /// <summary>
+    /// Initialize terrainData with worldSize, resolution and generate the height map
+    /// </summary>
+    private void Start()
+    {
+        terrainData.size = worldSize;
+
+        terrainData.heightmapResolution = resolution;
+
+        terrainData.SetHeights(0, 0, GenerateHeightArray(resolution));
+    }
+
+
+    /// <summary>
+    /// Generate the height array using the specified noise function
+    /// </summary>
+    /// <param name="res">resolution</param>
+    /// <param name="noiseFx">noise function</param>
+    /// <returns>a height array</returns>
+    private float[,] GenerateHeightArray(int res)
+    {
+        float[,] heightArray = new float[res, res];
+
+        for (int i = 0; i < res; ++i)
+        {
+            for (int j = 0; j < res; ++j)
+            {
+                heightArray[i, j] = GetPerlinNoise(i, j, res);
+            }
+        }
+
+        return heightArray;
+    }
+
+    /// <summary>
+    /// Generate perlin noise at specified coordinate and resolution
+    /// </summary>
+    /// <param name="i">current x position</param>
+    /// <param name="j">current y position</param>
+    /// <param name="res">resolution</param>
+    /// <returns></returns>
+    private float GetPerlinNoise(int x, int y, int res)
+    {
+        float xCoord = xOrigin + ScaleRamp(x, res) * scale;
+
+        float yCoord = yOrigin + ScaleRamp(y, res) * scale;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+
+    /// <summary>
+    /// Used to scale integer grid coordinate into float postion
+    /// </summary>
+    /// <param name="i">grid coordinate</param>
+    /// <param name="res">resolution</param>
+    /// <returns>A linear step size</returns>
+    private float ScaleRamp(int i, int res)
+    {
+        return (float)i / res;
+    }
+}
